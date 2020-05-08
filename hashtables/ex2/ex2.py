116c77126fa925cecacb407a926ecff4061eb5e1
Zac Higgins <zachiggins@Higgins-iMac.local>
2020-05-08 11:08:31 -0600

    working solution for reconstruct_trip

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -6,9 +6,23 @@ class Ticket:
 
 
 def reconstruct_trip(tickets, length):
-    
-    """
-    YOUR CODE HERE
-    """
-
+    legs = {}
+    route = [None] * length
+    for i in range(0, length):
+        current_ticket = tickets[i]
+        legs[current_ticket.source] = current_ticket.destination
+    route[0] = legs["NONE"]
+    route[1] = legs[route[0]]
+    if length > 2:
+        for i in range(2, length):
+            route[i] = legs[route[i - 1]]
     return route
+
+
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
+
+# tickets = [ticket_1, ticket_3, ticket_2]
+
+# reconstruct_trip(tickets, 3)
